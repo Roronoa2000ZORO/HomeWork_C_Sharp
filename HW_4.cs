607506f1a8f37d8a5f440644d8b66da24af0a284
Alikhanov Ibragim N <a01141226993n@gmail.com>
2023-07-01 21:33:21 +0300

    done

---
+++ HW_4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_C_Sharp
+{
+    struct Article
+    {
+        public int ProductCode;
+        public string ProductName;
+        public decimal ProductPrice;
+    }
+
+    struct Client
+    {
+        public int ClientCode;
+        public string FullName;
+        public string Address;
+        public string Phone;
+        public int OrderCount;
+        public decimal TotalOrderAmount;
+    }
+
+    struct RequestItem
+    {
+        public Article Product;
+        public int Quantity;
+    }
+
+    struct Request
+    {
+        public int OrderCode;
+        public Client Client;
+        public DateTime OrderDate;
+        public RequestItem[] OrderedItems;
+
+        public decimal TotalOrderAmount
+        {
+            get
+            {
+                decimal totalAmount = 0;
+                foreach (var item in OrderedItems)
+                {
+                    totalAmount += item.Product.ProductPrice * item.Quantity;
+                }
+                return totalAmount;
+            }
+        }
+    }
+    class HW_4
+    {
+    }
+}
